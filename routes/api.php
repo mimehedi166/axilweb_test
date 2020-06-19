6672daf58d9834048187370a28307b1383e8adb0
Mehedi <mimehedi166@gmail.com>
2020-06-19 19:43:39 +0600

    CORS POLICY FIXED

--- routes/api.php
+++ routes/api.php
@@ -15,6 +15,4 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('get-logs','CallLogsController@logDetails');
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+
