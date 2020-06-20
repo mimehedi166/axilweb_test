0783592f9a0e86c436b62c3b7784b27d8e1c95ae
Mehedi <mimehedi166@gmail.com>
2020-06-20 20:05:04 +0600

    Filter Added in GRID view

--- app/Repositories/Api/CallLogs/EloquentCallLogsRepository.php
+++ app/Repositories/Api/CallLogs/EloquentCallLogsRepository.php
@@ -8,10 +8,20 @@ class EloquentCallLogsRepository implements CallLogsRepository
     public function getDetails($request)
     {
         // TODO: Implement getDetails() method.
-        $query = CallLogs::orderBy("id","ASC");
-            if ($request->get('status'))
+        $query = CallLogs::orderBy("call_date","ASC");
+            if ($request->has('status') && !empty($request->get('status')))
             {
                 $query = $query->where('status', $request->get('status'));
+            }
+            if ($request->has('from_date') && !empty($request->get('from_date')))
+            {
+                $fromDate = $request->get('from_date');
+                $toDate = $request->get('from_date');
+                if ($request->has('to_date') && !empty($request->get('to_date')))
+                {
+                    $toDate = $request->get('to_date');
+                }
+                $query = $query->whereBetween('call_date', [$fromDate, $toDate]);
             }
         $query = $query->get()->toArray();
         return $query;
